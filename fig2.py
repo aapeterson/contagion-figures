a4068e2063745019babafb6c6712e5b4983bf97b
Andrew Peterson <andrew_peterson@brown.edu>
2020-11-30 15:53:04 -0500

    Initial commit.

---
+++ fig2.py
@@ -0,0 +1,377 @@
+#!/usr/bin/env python
+
+import mpmath
+import numpy as np
+import scipy.special
+from scipy.integrate import trapz
+from matplotlib import pyplot
+
+
+def makefig(figsize=(8., 4.), nrows=4, ncols_left=4, ncols_right=4,
+            lm=0.03, rm=0.01, hg=0.03, arrow_ratio=.5,
+            tm=0.08, bm=0.05, vg=0.03,
+            ):
+    """Custom axes layout for this figure."""
+    fig = pyplot.figure(figsize=figsize)
+    axes = np.ndarray((nrows, ncols_left + ncols_right), dtype=object)
+    axwidth = ((1. - lm - rm - hg * (ncols_left + ncols_right)) /
+               (ncols_left + ncols_right + arrow_ratio))
+    axheight = (1. - tm - bm - vg * (nrows - 1.)) / nrows
+    bottompoint = 1. - tm - axheight
+    for irow in range(nrows):
+        leftpoint = lm
+        for icol in range(ncols_left):
+            ax = fig.add_axes((leftpoint, bottompoint, axwidth, axheight))
+            axes[irow, icol] = ax
+            leftpoint += axwidth + hg
+        leftpoint += hg + arrow_ratio * axwidth
+        for icol in range(ncols_right):
+            ax = fig.add_axes((leftpoint, bottompoint, axwidth, axheight))
+            axes[irow, icol + ncols_left] = ax
+            leftpoint += axwidth + hg
+        bottompoint -= axheight + vg
+    # Arrow axes.
+    bottompoint = 0. + bm
+    leftpoint = lm + axwidth * ncols_left + hg * ncols_left
+    arrow_width = arrow_ratio * axwidth
+    arrow_height = axheight * nrows + vg * (nrows - 1.)
+    ax_arrow = fig.add_axes((leftpoint, bottompoint,
+                             arrow_width, arrow_height))
+    return fig, axes, ax_arrow
+
+
+class Evolution:
+    """Evolves a distribution with chi, where phi=beta*chi.
+    The initial distribution must be given by initial_distribution
+    which takes in epsilon and outputs the distribution value.
+    """
+    def __init__(self, initial_distribution, beta=1.):
+        self.dist0 = initial_distribution
+        self.beta = beta
+
+    def __call__(self, epsilon, chi):
+        """Returns the value of the evolved distribution at
+        'time' chi and value epsilon."""
+        fxn = self.dist0(epsilon)
+        exponential = np.exp(-self.beta * chi * epsilon)
+        return fxn * exponential
+
+    def get_mean(self, chi, epsilon_range):
+        """At 'time' chi, returns the mean epsilon of the distribution
+        over the range in epsilon_range (a list of numbers from min
+        to max that gives the spacing for numerical integration).
+        Does it numerically with scipy's trapz; that is, the trapezoid
+        rule."""
+        epsilon_range = np.array(epsilon_range)
+        ns = np.array([self(epsilon, chi) for epsilon in epsilon_range])
+        num = trapz(ns * epsilon_range, epsilon_range)
+        den = trapz(ns, epsilon_range)
+        return num / den, den
+
+
+class Gamma:
+    """Creates callable gamma function with k value specified."""
+    def __init__(self, k=1., Ns=1000.):
+        self.k = k
+        self.Ns = Ns
+
+    def __call__(self, epsilon):
+        k = self.k
+        mean0 = 1.
+        pdf = self.Ns * epsilon**(k - 1.) * np.exp(-k * epsilon / mean0)
+        pdf /= (mean0 / k)**k * scipy.special.gamma(k)
+        return pdf
+
+
+class Beta:
+    """Creates callable beta function with alpha, beta values specified."""
+    def __init__(self, alpha, beta, Ns=1000.):
+        self.alpha = alpha
+        self.beta = beta
+        self.Ns = Ns
+        gamma = scipy.special.gamma
+        self.gamma_factor = gamma(alpha + beta) / gamma(alpha) / gamma(beta)
+        mean = alpha / (alpha + beta)
+        self.scale = 1. / mean  # Will be used to set the initial mean to 1.
+        self.max_value = self.scale  # Upper bound.
+        self.mean = mean
+
+    def __call__(self, x):
+        alpha, beta, scale = self.alpha, self.beta, self.scale
+        y = x / scale
+        pdf = y**(alpha - 1.) * (1. - y)**(beta - 1.) * self.gamma_factor
+        pdf /= scale
+        return pdf * self.Ns
+
+    def getK(self, phi):
+        """Returns the apparent order paramter K at a given "time" phi."""
+        alpha, beta = self.alpha, self.beta
+        num = 1. + alpha
+        num *= reghyp1f1(alpha, alpha + beta, -phi)
+        num *= reghyp1f1(2. + alpha, 2. + alpha + beta, -phi)
+        den = alpha * reghyp1f1(1. + alpha, 1. + alpha + beta, -phi)**2
+        K = -1 + num / den
+        return K
+
+
+def reghyp1f1(a, b, z):
+    """Regularized Hypergeometric 1F1 function."""
+    return float(mpmath.hyp1f1(a, b, z) / mpmath.gamma(b))
+
+
+fig, axes, ax_arrow = makefig()
+
+# Common figure information.
+fill_color = '0.8'
+labels = {}
+meanheight = 0.2
+ax_number = -1
+chis = [0., 0.5, 2., 8.]
+letters = 'ABCDEFGHIJKLMNOP'
+betalabel = '$\mathbf{{{:s}}}$ beta\n$a$={:g},$b$={:g}'
+gammalabel = '$\mathbf{{{:s}}}$ gamma\n $k$={:g}'
+ytop = 1050.  # top ylim
+
+#############
+# Beta plots.
+#############
+
+ax_number += 1
+a, b = 0.25, 35.
+labels[ax_number] = betalabel.format(letters[ax_number], a, b)
+beta = Beta(a, b)
+evolve = Evolution(beta)
+local_axes = axes[:, ax_number]
+for chi, ax in zip(chis, local_axes):
+    epsilons = np.linspace(0., 2., num=100)
+    ys = [evolve(epsilon, chi) for epsilon in epsilons]
+    ax.plot(epsilons, ys)
+    ax.fill_between(epsilons, ys, color=fill_color)
+    ax.set_xlim(0., 2.)
+    epsilons = np.logspace(np.log10(1e-8), np.log10(beta.max_value), num=1000)
+    mean, count = evolve.get_mean(chi, epsilons)
+    k = 1. / beta.getK(1000.)
+    fits = [(count / scipy.special.gamma(k) / (mean / k)**k * epsilon**(k-1) *
+             np.exp(-epsilon * k / mean)) for epsilon in epsilons]
+    ax.plot(epsilons, fits, ':')
+    ax.set_ylim(0., ytop)
+    ylim = ax.get_ylim()
+    ax.plot([mean] * 2, [0., ylim[1] * meanheight], '-', color='0.5')
+    ax.set_ylim(ylim)
+
+ax_number += 1
+a, b = .5, .5
+labels[ax_number] = betalabel.format(letters[ax_number], a, b)
+beta = Beta(a, b)
+evolve = Evolution(beta)
+local_axes = axes[:, ax_number]
+ylim = None
+for chi, ax in zip(chis, local_axes):
+    epsilons = np.linspace(0., 2., num=100)
+    ys = [evolve(epsilon, chi) for epsilon in epsilons]
+    ax.plot(epsilons, ys)
+    ax.fill_between(epsilons, ys, color=fill_color)
+    ax.set_xlim(0., epsilons[-1])
+    if ylim is None:
+        ylim = ax.get_ylim()
+    epsilons = np.logspace(np.log10(1e-8), np.log10(beta.max_value / 2.))
+    upper_epsilons = beta.max_value - np.flip(epsilons)[:-1]
+    epsilons = np.concatenate((epsilons, upper_epsilons))
+    mean, count = evolve.get_mean(chi, epsilons)
+    k = 1. / beta.getK(1000.)
+    fits = [(count / scipy.special.gamma(k) / (mean / k)**k * epsilon**(k-1) *
+             np.exp(-epsilon * k / mean)) for epsilon in epsilons]
+    ax.plot(epsilons, fits, ':')
+    ax.set_ylim(0., ytop)
+    ylim = ax.get_ylim()
+    ax.plot([mean] * 2, [0., ylim[1] * meanheight], '-', color='0.5')
+    ax.set_ylim(ylim)
+
+ax_number += 1
+a, b = 1., 1.
+labels[ax_number] = betalabel.format(letters[ax_number], a, b)
+beta = Beta(a, b)
+evolve = Evolution(beta)
+local_axes = axes[:, ax_number]
+for chi, ax in zip(chis, local_axes):
+    epsilons = np.linspace(0., 2., num=100)
+    ys = [evolve(epsilon, chi) for epsilon in epsilons]
+    ax.plot(epsilons, ys)
+    ax.fill_between(epsilons, ys, color=fill_color)
+    ax.set_xlim(0., epsilons[-1])
+    mean, count = evolve.get_mean(chi, epsilons)
+    fits = [count / mean * np.exp(-epsilon / mean) for epsilon in epsilons]
+    k = 1. / beta.getK(1000.)
+    fits = [(count / scipy.special.gamma(k) / (mean / k)**k * epsilon**(k-1) *
+             np.exp(-epsilon * k / mean)) for epsilon in epsilons]
+    ax.plot(epsilons, fits, ':')
+    ax.set_ylim(0., ytop)
+    ylim = ax.get_ylim()
+    ax.plot([mean] * 2, [0., ylim[1] * meanheight], '-', color='0.5')
+    ax.set_ylim(ylim)
+
+ax_number += 1
+a, b = 2., 2.
+labels[ax_number] = betalabel.format(letters[ax_number], a, b)
+beta = Beta(a, b)
+evolve = Evolution(beta)
+epsilons = np.linspace(0., beta.scale)
+local_axes = axes[:, ax_number]
+for chi, ax in zip(chis, local_axes):
+    epsilons = np.linspace(0., 2., num=100)
+    ys = [evolve(epsilon, chi) for epsilon in epsilons]
+    ax.plot(epsilons, ys)
+    ax.fill_between(epsilons, ys, color=fill_color)
+    ax.set_xlim(0., epsilons[-1])
+    mean, count = evolve.get_mean(chi, epsilons)
+    k = 1. / beta.getK(1000.)
+    fits = [(count / scipy.special.gamma(k) / (mean / k)**k * epsilon**(k-1) *
+             np.exp(-epsilon * k / mean)) for epsilon in epsilons]
+    ax.plot(epsilons, fits, ':')
+    ax.set_ylim(0., ytop)
+    ylim = ax.get_ylim()
+    ax.plot([mean] * 2, [0., ylim[1] * meanheight], '-', color='0.5')
+    ax.set_ylim(ylim)
+
+##############
+# Gamma plots.
+##############
+
+ax_number += 1
+k = 0.25
+labels[ax_number] = gammalabel.format(letters[ax_number], k)
+evolve = Evolution(Gamma(k=k))
+local_axes = axes[:, ax_number]
+for chi, ax in zip(chis, local_axes):
+    epsilons = np.linspace(0., 2., num=100)
+    ys = [evolve(epsilon, chi) for epsilon in epsilons]
+    ax.plot(epsilons, ys)
+    ax.fill_between(epsilons, ys, color=fill_color)
+    ax.set_xlim(0., epsilons[-1])
+    ax.plot(epsilons, ys, ':')
+    epsilons = np.logspace(np.log10(1e-8), np.log10(100.), num=1000)
+    mean, count = evolve.get_mean(chi, epsilons)
+    ax.set_ylim(0., ytop)
+    ylim = ax.get_ylim()
+    ax.plot([mean] * 2, [0., ylim[1] * meanheight], '-', color='0.5')
+    ax.set_ylim(ylim)
+    ax.set_xlim(0., 2.)
+
+ax_number += 1
+k = 0.5
+labels[ax_number] = gammalabel.format(letters[ax_number], k)
+evolve = Evolution(Gamma(k=k))
+local_axes = axes[:, ax_number]
+for chi, ax in zip(chis, local_axes):
+    epsilons = np.linspace(0., 2., num=100)
+    ys = [evolve(epsilon, chi) for epsilon in epsilons]
+    ax.plot(epsilons, ys)
+    ax.fill_between(epsilons, ys, color=fill_color)
+    ax.set_xlim(0., epsilons[-1])
+    ax.plot(epsilons, ys, ':')
+    epsilons = np.logspace(np.log10(1e-8), np.log10(100.), num=1000)
+    mean, count = evolve.get_mean(chi, epsilons)
+    ax.set_ylim(0., ytop)
+    ylim = ax.get_ylim()
+    ax.plot([mean] * 2, [0., ylim[1] * meanheight], '-', color='0.5')
+    ax.set_ylim(ylim)
+    ax.set_xlim(0., 2.)
+
+ax_number += 1
+k = 1.0
+labels[ax_number] = gammalabel.format(letters[ax_number], k)
+evolve = Evolution(Gamma(k=k))
+local_axes = axes[:, ax_number]
+for chi, ax in zip(chis, local_axes):
+    epsilons = np.linspace(0., 2., num=100)
+    ys = [evolve(epsilon, chi) for epsilon in epsilons]
+    ax.plot(epsilons, ys)
+    ax.fill_between(epsilons, ys, color=fill_color)
+    ax.set_xlim(0., epsilons[-1])
+    ax.plot(epsilons, ys, ':')
+    epsilons = np.logspace(np.log10(1e-8), np.log10(100.), num=1000)
+    mean, count = evolve.get_mean(chi, epsilons)
+    ax.set_ylim(0., ytop)
+    ylim = ax.get_ylim()
+    ax.plot([mean] * 2, [0., ylim[1] * meanheight], '-', color='0.5')
+    ax.set_ylim(ylim)
+    ax.set_xlim(0., 2.)
+
+ax_number += 1
+k = 2.0
+labels[ax_number] = gammalabel.format(letters[ax_number], k)
+evolve = Evolution(Gamma(k=k))
+local_axes = axes[:, ax_number]
+for chi, ax in zip(chis, local_axes):
+    epsilons = np.linspace(0., 2., num=100)
+    ys = [evolve(epsilon, chi) for epsilon in epsilons]
+    ax.plot(epsilons, ys)
+    ax.fill_between(epsilons, ys, color=fill_color)
+    ax.set_xlim(0., epsilons[-1])
+    ax.plot(epsilons, ys, ':')
+    epsilons = np.logspace(np.log10(1e-8), np.log10(100.), num=1000)
+    mean, count = evolve.get_mean(chi, epsilons)
+    ax.set_ylim(0., ytop)
+    ylim = ax.get_ylim()
+    ax.plot([mean] * 2, [0., ylim[1] * meanheight], '-', color='0.5')
+    ax.set_ylim(ylim)
+    ax.set_xlim(0., 2.)
+
+for ax in axes.ravel():
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+# Annotations.
+axes[0, 0].annotate(
+    s=r'$\bar{\varepsilon}_0$',
+    xy=(1.0, ytop*(meanheight*1.1)), xycoords='data',
+    xytext=(1.5, ytop*(meanheight*2.0)), textcoords='data',
+    arrowprops={'arrowstyle': '->',
+                'color': 'k',
+                'connectionstyle': 'arc3,rad=0.3'})
+axes[1, 0].annotate(
+    s=r'$\bar{\varepsilon}(t)$',
+    xy=(0.3, ytop*(meanheight*1.1)), xycoords='data',
+    xytext=(1.3, ytop*(meanheight*2.0)), textcoords='data',
+    arrowprops={'arrowstyle': '->',
+                'color': 'k',
+                'connectionstyle': 'arc3,rad=0.3'})
+axes[1, 2].annotate(
+    s=r'gamma',
+    xy=(0.2, ytop*0.6), xycoords='data',
+    xytext=(0.9, ytop*0.7), textcoords='data',
+    arrowprops={'arrowstyle': '->',
+                'color': 'C1',
+                'connectionstyle': 'arc3,rad=0.3'})
+
+# Captions, cleanup, etc.
+for ax_number, label in labels.items():
+    box = axes[0, ax_number].get_position()
+    x = np.average((box.x0, box.x1))
+    y = box.y1 + 0.07
+    fig.text(x, y, label, ha='center', va='top', fontsize=9.)
+
+axes[0, 0].set_ylabel(r'$n_\mathrm{S}(\varepsilon,0)$')
+for number, ax in enumerate(axes[1:, 0]):
+    ax.set_ylabel(r'$n_\mathrm{{S}}(\varepsilon,t_{:d})$'.format(number + 1))
+for ax in axes[-1, :]:
+    ax.set_xlabel(r'$\varepsilon$')
+
+bbox_props = dict(boxstyle="rarrow,pad=0.3", fc='0.9',  ec="k", lw=1.0)
+ax_arrow.text(0.5, 0,
+              r'increasing time [$\phi(t)$]',
+              ha="center", va="center", rotation=-90,
+              bbox=bbox_props)
+ax_arrow.set_ylim(1., -1.)
+ax_arrow.set_xlim(0., 1.)
+
+ax_arrow.spines['top'].set_visible(False)
+ax_arrow.spines['bottom'].set_visible(False)
+ax_arrow.spines['left'].set_visible(False)
+ax_arrow.spines['right'].set_visible(False)
+ax_arrow.set_xticks([])
+ax_arrow.set_yticks([])
+
+fig.savefig('fig2.pdf')
---
+++ fig3.py
@@ -0,0 +1,339 @@
+import scipy.special
+import numpy as np
+import mpmath
+from matplotlib import pyplot
+from scipy.integrate import solve_ivp
+from scipy.interpolate import interp1d
+
+
+class Gamma:
+    """Creates a gamma distribution. mean is the mean of the initial distribution,
+    and k is the shape parameter."""
+    def __init__(self, mean, k):
+        self.mean0 = mean
+        self.k = k
+
+    def __call__(self, epsilon):
+        Ns = 1000.
+        return Ns * epsilon**(self.k - 1.) * np.exp(-self.k * epsilon)
+
+    def get_ebar(self, ki, C):
+        """Returns the mean value of the distribution when a cumulative infection
+        pressure of C has occured, with rate constant ki."""
+        return 1. / (1. / self.mean0 + ki * C / self.k)
+
+    def get_distribution(self, ki, C, epsilon, xS=1.):
+        """Returns the value of the distribution at value epsilon,
+        given the cumulative infection pressure of C and infection rate ki.
+        if xS=1., returns the normalized distribution; you can alternately
+        provide xS to see the shrinking distribution."""
+        k, mean0 = self.k, self.mean0
+        nS = (k / mean0 + ki * C)**k
+        nS *= epsilon**(k - 1)
+        nS *= np.exp(- (k / mean0 + ki * C) * epsilon)
+        nS /= scipy.special.gamma(k)
+        return nS
+
+
+class Beta:
+    """Creates callable beta function with alpha, beta values specified."""
+    def __init__(self, alpha, beta, Ns=1000.):
+        self.alpha = alpha
+        self.beta = beta
+        self.Ns = Ns
+        mean = alpha / (alpha + beta)
+        self.scale = 1. / mean  # Will be used to set the initial mean to 1.
+        self.max_value = self.scale  # Upper bound.
+        self.mean = mean
+
+    def __call__(self, x):
+        alpha, beta, scale = self.alpha, self.beta, self.scale
+        y = x / scale
+        pdf = y**(alpha - 1.) * (1. - y)**(beta - 1.)
+        pdf *= scipy.special.beta(alpha, beta)
+        pdf /= scale
+        return pdf * self.Ns
+
+    def getK(self, phi):
+        """Returns the apparent order paramter K at a given "time" phi."""
+        alpha, beta, scale = self.alpha, self.beta, self.scale
+        num = 1. + alpha
+        num *= reghyp1f1(alpha, alpha + beta, -phi * scale)
+        num *= reghyp1f1(2. + alpha, 2. + alpha + beta, -phi * scale)
+        den = alpha * reghyp1f1(1. + alpha, 1. + alpha + beta, -phi * scale)**2
+        K = -1 + num / den
+        return K
+
+    def get_ebar(self, ki, C):
+        """Returns the mean value of the distribution when a cumulative infection
+        pressure of C has occured, with rate constant ki."""
+        phi = ki * C
+        alpha, beta, scale = self.alpha, self.beta, self.scale
+        epbar = alpha * reghyp1f1(1. + alpha, 1. + alpha + beta, -phi * scale)
+        epbar /= reghyp1f1(alpha, alpha + beta, -phi * scale)
+        epbar *= scale
+        return epbar
+
+
+def reghyp1f1(a, b, z):
+    """Regularized Hypergeometric 1F1 function."""
+    return float(mpmath.hyp1f1(a, b, z) / mpmath.gamma(b))
+
+
+def get_power_results(power, ki, kd, times, xI0):
+    """Perform the numerical integration with the power-law model,
+    with power being the order of the rate term with respect to xS."""
+    dydt = MakePower_dydt(ki, kd, power)
+    answer = solve_ivp(fun=dydt, t_span=(times[0], times[-1]),
+                       y0=[1. - xI0, xI0], dense_output=True)
+    return answer.sol(times)
+
+
+class MakePower_dydt:
+    """Makes a callable function to provide dydt for our system of equation,
+    under the power-law model, where power is typically 1 or 2 to indicate
+    the order of the rate with respect to xS."""
+    def __init__(self, ki, kd, power):
+        self.ki = ki
+        self.kd = kd
+        self.power = power
+
+    def __call__(self, t, y):
+        xS, xI = y
+        rate = self.ki * xS**self.power * xI
+        dxS_dt = - rate
+        dxI_dt = rate - self.kd * xI
+        return [dxS_dt, dxI_dt]
+
+
+def get_gamma_results(k, ki, kd, times, xI0):
+    """Perform the numerical integration with the gamma distribution,
+    with shape parameter k."""
+    distribution = Gamma(mean=1., k=k)
+    dydt = MakeDistro_dydt(ki, kd, distribution.get_ebar)
+    answer = solve_ivp(fun=dydt, t_span=(times[0], times[-1]),
+                       y0=[1. - xI0, xI0, 0.], dense_output=True)
+    return answer.sol(times)
+
+
+def get_beta_results(alpha, beta, ki, kd, times, xI0):
+    """Perform the numerical integration with the uniform distribution."""
+    distribution = Beta(alpha=alpha, beta=beta)
+    dydt = MakeDistro_dydt(ki, kd, distribution.get_ebar)
+    answer = solve_ivp(fun=dydt, t_span=(times[0], times[-1]),
+                       y0=[1. - xI0, xI0, 0.], dense_output=True)
+    return answer.sol(times)
+
+
+class MakeDistro_dydt:
+    """Makes a callable function to provide dydt for our system of equations.
+    Here, I'm using ki and kd to indicate the infection and decay rate
+    constants (to avoid using confusing ourselves with two things called
+    gamma!).  get_ebar is a function that returns the current mean value of
+    the distribution given ki and C, where C is the cumulative infection
+    pressure.
+    """
+    def __init__(self, ki, kd, get_ebar):
+        self.ki = ki
+        self.kd = kd
+        self.get_ebar = get_ebar
+
+    def __call__(self, t, y):
+        xS, xI, C = y
+        ebar = self.get_ebar(self.ki, C)
+        rate = self.ki * ebar * xI * xS
+        dxS_dt = - rate
+        dxI_dt = rate - self.kd * xI
+        dC_dt = xI
+        return [dxS_dt, dxI_dt, dC_dt]
+
+
+fig, axes = pyplot.subplots(ncols=2, figsize=(8., 4.))
+fig.subplots_adjust(left=0.08, right=0.99, top=0.80)
+dax, kax = axes
+linestyles = (':', '--', '-.', (0, (3, 1, 1, 1)))
+
+phis = np.linspace(0., 8., num=1000)
+
+################
+# Dynamics plot.
+################
+
+ki = 2.  # R0
+kd = 1.  # making this unitless
+xI0 = 0.0001  # initial infected population; rest assumed susceptible
+times = np.linspace(0., 20., num=1000)
+final_phis = {}  # For use in order-parameter plot.
+
+# Power models.
+for power in [1., 1.5, 2., 3., 5.]:
+    ys = get_power_results(power, ki, kd, times, xI0)
+    dax.plot(times, 1. - ys[0], lw=6., color='0.8')
+    dax.text(times[-1] + 1.7, 1. - ys[0, -1],
+             r'$\beta I S^{{{:g}}}$'.format(power), va='center')
+
+# Homogeneous model.
+power = 1.
+ys = get_power_results(power, ki, kd, times, xI0)
+dax.plot(times, 1. - ys[0], lw=1., color='k', label='homogeneous')
+
+# Gamma models.
+kax.set_prop_cycle(None)
+for k in [0.25, 0.5, 1., 2.]:
+    ys = get_gamma_results(k, ki, kd, times, xI0)
+    final_phis['gamma,{:g}'.format(k)] = ys[-1][-1] * ki
+    dax.plot(times, 1. - ys[0])
+
+# Beta models.
+dax.set_prop_cycle(None)
+linestyle = iter(linestyles)
+
+for a, b in [(0.25, 35.), (0.5, 0.5), (1., 1.), (2., 2.)]:
+    ys = get_beta_results(a, b, ki, kd, times, xI0)
+    final_phis['beta,{:g},{:g}'.format(a, b)] = ys[-1][-1] * ki
+    curve = dax.plot(times, 1. - ys[0], linestyle=next(linestyle))[0]
+    dax.plot(times[-1], 1. - ys[0][-1], '.', color=curve.get_color())
+
+# Clean up, captions, etc.
+dax.set_ylabel('cumulative infected ($I + R$)')
+dax.set_xlabel('time, dimensionless ($t/\gamma$)')
+
+dax.set_xlim(0., 25.)
+dax.set_ylim(bottom=0.)
+
+#############
+# Order plot.
+#############
+
+linestyle = iter(linestyles)
+betacurves = []
+
+# Beta models.
+a, b = .25, 35.
+beta = Beta(alpha=a, beta=b)
+Ks = [beta.getK(phi) for phi in phis]
+curve = kax.plot(phis, Ks, linestyle=next(linestyle),
+                 label='Beta: $a$={:g},$b$={:g} (gamma-like)'
+                       .format(a, b))[0]
+betacurves += [curve]
+f = interp1d(phis, Ks)
+final_phi = final_phis['beta,{:g},{:g}'.format(a, b)]
+kax.plot(final_phi, f(final_phi), '.', color=curve.get_color())
+kax.annotate('gamma-like',
+             xy=(0.1, f(0.1)), xycoords='data',
+             xytext=(0.1 - 0.10, f(0.1) - 0.8), textcoords='data',
+             arrowprops={'arrowstyle': '->',
+                         'color': curve.get_color(),
+                         'connectionstyle': 'arc3,rad=0.3'})
+
+a, b = .5, .5
+beta = Beta(alpha=a, beta=b)
+Ks = [beta.getK(phi) for phi in phis]
+curve = kax.plot(phis, Ks, linestyle=next(linestyle),
+                 label='Beta: $a$={:g},$b$={:g} (gamma-like)'
+                       .format(a, b))[0]
+betacurves += [curve]
+f = interp1d(phis, Ks)
+final_phi = final_phis['beta,{:g},{:g}'.format(a, b)]
+kax.plot(final_phi, f(final_phi), '.', color=curve.get_color())
+kax.annotate('bimodal',
+             xy=(1.8, f(1.8)), xycoords='data',
+             xytext=(1.8 - 0.10, f(1.8) - 0.5), textcoords='data',
+             arrowprops={'arrowstyle': '->',
+                         'color': curve.get_color(),
+                         'connectionstyle': 'arc3,rad=0.3'})
+
+a, b = 1., 1.
+beta = Beta(alpha=a, beta=b)
+Ks = [beta.getK(phi) for phi in phis]
+curve = kax.plot(phis, Ks, linestyle=next(linestyle),
+                 label='Beta: $a$={:g},$b$={:g} (gamma-like)'
+                       .format(a, b))[0]
+betacurves += [curve]
+f = interp1d(phis, Ks)
+final_phi = final_phis['beta,{:g},{:g}'.format(a, b)]
+kax.plot(final_phi, f(final_phi), '.', color=curve.get_color())
+kax.annotate('uniform',
+             xy=(2.5, f(2.1)), xycoords='data',
+             xytext=(2.1 + 1.80, f(2.1) - 0.1), textcoords='data',
+             arrowprops={'arrowstyle': '->',
+                         'color': curve.get_color(),
+                         'connectionstyle': 'arc3,rad=-0.1'})
+
+a, b = 2., 2.
+beta = Beta(alpha=a, beta=b)
+Ks = [beta.getK(phi) for phi in phis]
+curve = kax.plot(phis, Ks, linestyle=next(linestyle),
+                 label='Beta: $a$={:g},$b$={:g} (gamma-like)'
+                       .format(a, b))[0]
+betacurves += [curve]
+f = interp1d(phis, Ks)
+final_phi = final_phis['beta,{:g},{:g}'.format(a, b)]
+kax.plot(final_phi, f(final_phi), '.', color=curve.get_color())
+kax.annotate('unimodal',
+             xy=(2.1 + 0.3, f(2.1)), xycoords='data',
+             xytext=(2.1 + 1.80, f(2.1) - 0.25), textcoords='data',
+             arrowprops={'arrowstyle': '->',
+                         'color': curve.get_color(),
+                         'connectionstyle': 'arc3,rad=-0.1'})
+
+# Gamma models.
+kax.set_prop_cycle(None)
+gammacurves = []
+for k in [0.25, 0.5, 1., 2.]:
+    c = kax.plot([phis[0], phis[-1]], [1. / k]*2, '-',
+                 label='Gamma: $k$={:g}'.format(k))[0]
+    gammacurves += [c]
+
+# Homogeneous model.
+kax.plot([phis[0], phis[-1]], [0.]*2, '-k')
+kax.annotate('homogeneous',
+             xy=(1., 0.), xycoords='data',
+             xytext=(2.1,  -0.4), textcoords='data',
+             arrowprops={'arrowstyle': '->',
+                         'color': 'k',
+                         'connectionstyle': 'arc3,rad=-0.2'})
+
+
+# Captions and cleanup.
+kax.set_ylabel('$\mathcal{K}(t)$, general order parameter')
+kax.set_xlabel('$\phi(t)$, progress variable')
+kax.set_ylim(-0.7, 4.5)
+kax.set_yticks(range(0, 5))
+
+dax.text(0.07, 0.95, 'A', fontsize=12., weight='bold', transform=dax.transAxes,
+         ha='center', va='center')
+kax.text(0.07, 0.95, 'B', fontsize=12., weight='bold', transform=kax.transAxes,
+         ha='center', va='center')
+
+
+def add_entry(column, y, label, style, color, lw=1.):
+    """Custom legend."""
+    lax.plot([column, column + 0.2], [y]*2, style, color=color, lw=lw)
+    lax.text(column + 0.25, y, label, va='center', fontsize=9.)
+
+
+lax = fig.add_axes((0.1, 0.83, 0.8, 0.99 - 0.83))
+y = 0.
+add_entry(0., y, 'homogeneous', '-', 'k')
+y -= 1.
+add_entry(0., y, 'power law', '-', '0.8', 6.)
+y = 1.
+for curve in betacurves:
+    y -= 1.
+    add_entry(1.7, y, curve.get_label(), curve.get_linestyle(),
+              curve.get_color(), lw=2.0)
+y = 1.
+for curve in gammacurves:
+    y -= 1.
+    add_entry(0.8, y, curve.get_label(), curve.get_linestyle(),
+              curve.get_color(), lw=2.0)
+
+lax.set_xlim(-0.1, 3.)
+lax.set_ylim(-3.5, 0.5)
+lax.set_xticks([])
+lax.set_yticks([])
+for spine in lax.spines.values():
+    spine.set_edgecolor('0.8')
+
+fig.savefig('fig3.pdf')
